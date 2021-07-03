3b1ea7249cbfd209b6f9f2c5dbe16d673deddc63
tuanna <kullnobita69@gmail.com>
2021-07-03 11:51:52 +0700

    update title and gender questions

--- src/components/gender/Gender.js
+++ src/components/gender/Gender.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classnames from 'classnames';
+
+import './Gender.scss';
 
 const Gender = ({ triggerNextStep, trigger }) => {
-  const handleClick = () => {
+  const genderOptions = {
+    MALE: 'male',
+    FEMALE: 'female',
+  };
+
+  const [hideMale, setHideMale] = useState(false);
+  const [hideFemale, setHideFemale] = useState(false);
+
+  const handleClick = (gender) => {
+    gender === genderOptions.MALE ? setHideFemale(true) : setHideMale(true);
     triggerNextStep({ value: '', trigger });
   };
+
   return (
-    <div>
-      <button onClick={handleClick}>Male</button>
-      <button onClick={handleClick}>Femail</button>
+    <div className="Gender">
+      <button
+        className={classnames({ hide: hideMale })}
+        onClick={() => handleClick(genderOptions.MALE)}
+      >
+        Male
+      </button>
+      <button
+        className={classnames({ hide: hideFemale })}
+        onClick={() => handleClick(genderOptions.FEMALE)}
+      >
+        Femail
+      </button>
     </div>
   );
 };
