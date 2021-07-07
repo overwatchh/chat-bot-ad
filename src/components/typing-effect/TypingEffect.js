f1cf9a995fd3f46e36eabe98f8d5ee8fe57d9b45
tuanna <kullnobita69@gmail.com>
2021-07-07 22:06:47 +0700

    natural typing speed

--- src/components/typing-effect/TypingEffect.js
+++ src/components/typing-effect/TypingEffect.js
@@ -5,7 +5,7 @@ function TypingEffect({ message, trigger, triggerNextStep }) {
     <Typewriter
       options={{
         cursor: "",
-        delay: 0,
+        delay: "natural",
       }}
       onInit={(typewriter) => {
         typewriter
