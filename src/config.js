8be23e9e452ae27d223578ded533c1ac6dc5bab6
tuanna <kullnobita69@gmail.com>
2021-07-19 16:34:04 +0700

    add loc sub-domain

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const adManagers = [
     desc: "Typing Effect",
     path: "/",
     landingPageLink:
-      "https://www.luckycharms.store/buy-now?utm_source=434512&utm_campaign=04881E&utm_content=9D50D5&utm_medium=3AE05F",
+      "https://www.phazodiac.online/bai-dich-1?utm_source=ED39ED&utm_campaign=5CA1D9&utm_content=DC466D&utm_medium=8C6E13",
     isTypingEffect: false,
   },
 ];
