1dfe938cc8016fc54f161f4eeaaa9ab75994b490
tuanna <kullnobita69@gmail.com>
2021-07-29 14:28:50 +0700

    add thuySubdomain

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const adManagers = [
     desc: "Typing Effect",
     path: "/",
     landingPageLink:
-      "https://www.horuseye.online/287cuoi?utm_source=62243E&utm_campaign=525298&utm_content=7C0DFE&utm_medium=D1186D",
+      "https://www.mczodiac21.live/ncuoi?fbclid=IwAR0yOH8H75ckWJidp6ovvXsu64-cyNriR9tzpcrRVI3ZI5Y_VPmRPpo-t_g",
     isTypingEffect: false,
   },
 ];
