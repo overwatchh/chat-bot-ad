6f0bc17cbc607c57a7d9a2c76c3c8319a2516508
tuanna <kullnobita69@gmail.com>
2021-07-29 11:43:51 +0700

    add phuongSubdomain

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const adManagers = [
     desc: "Typing Effect",
     path: "/",
     landingPageLink:
-      "https://www.phazodiac.online/bai-dich-1?utm_source=ED39ED&utm_campaign=5CA1D9&utm_content=DC466D&utm_medium=8C6E13",
+      "https://www.horuseye.online/287cuoi?utm_source=62243E&utm_campaign=525298&utm_content=7C0DFE&utm_medium=D1186D",
     isTypingEffect: false,
   },
 ];
