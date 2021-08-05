95635501858b84cb16df84807c852c38bc8a2ed4
tuan <kullnobita69@gmail.com>
2021-08-05 22:24:09 +0700

    test push data to ERP

--- src/components/BuyNowButton/index.js
+++ src/components/BuyNowButton/index.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./BuyNow.scss";
 import ReactGA from "react-ga";
+import axios from "axios";
 function BuyNowButton({ title, href }) {
   const clickHandler = () => {
+    console.log("loading");
     ReactGA.event({
       category: "Buy now ",
       action: "Go to landing page",
     });
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: "Token 8c9d6a00f474ff78666ece47b9cb3102cec18e0e",
+    };
+    const endpoint =
+      "https://azjewelery.perob.vn/crm/v1/single_product_potential_customers/";
+    const data = {
+      name: "Nguyen Van A",
+      phone: "0999999999",
+      address: "address",
+      gender: 0,
+      message: "content",
+      product_code: "AZ16004",
+    };
+    axios
+      .post(endpoint, data, {
+        headers: headers,
+      })
+      .then((response) => {
+        console.log("response", response);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
   };
-  return (
-    <a onClick={clickHandler} target="_blank" href={href} class="BuyNowButton">
-      {title}
-    </a>
-  );
+  return <div onClick={clickHandler}>{title}</div>;
 }
 
 export default BuyNowButton;
--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const adManagers = [
     path: "/",
     landingPageLink:
       "https://www.zodiacnews.website/b3?utm_source=F0C3F9&utm_campaign=AE4EF6&utm_content=6EEEF7&utm_medium=D038F5",
-    isTypingEffect: true,
+    isTypingEffect: false,
   },
   {
     desc: "Turn off typing effect",
